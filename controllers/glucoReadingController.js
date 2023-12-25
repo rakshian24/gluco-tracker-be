1f915a62418ebe0d8f9f96ee16d0397e0b07dffb
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2023-12-25 15:19:23 +0530

    feat: added foodConsumedAt field for glucoReading schema

--- controllers/glucoReadingController.js
+++ controllers/glucoReadingController.js
@@ -4,7 +4,11 @@ import moment from "moment";
 import User from "../models/userModel.js";
 import GlucoReading from "../models/glucoReadingModel.js";
 import Food from "../models/foodModel.js";
-import { AppError, getFormattedTimeStamp, isArrayEmpty } from "../utils/index.js";
+import {
+  AppError,
+  getFormattedTimeStamp,
+  isArrayEmpty,
+} from "../utils/index.js";
 
 // description  Adding glucose reading data
 // route        POST /api/v1/glucoseReading
@@ -19,7 +23,9 @@ const createReading = asyncHandler(async (req, res) => {
       consumedFoods,
       description,
       isMedsTaken = null,
-      isExercised } = req.body;
+      isExercised,
+      foodConsumedAt,
+    } = req.body;
 
     //TODO: add logic to add only one value (BB, AB, BL, AL, BD, AD) for the day;
 
@@ -30,7 +36,8 @@ const createReading = asyncHandler(async (req, res) => {
       description,
       isExercised,
       isMedsTaken,
-      userId: user._id
+      foodConsumedAt,
+      userId: user._id,
     });
 
     res.status(201).json({
@@ -38,16 +45,16 @@ const createReading = asyncHandler(async (req, res) => {
         _id: newReading._id,
         reading: newReading.reading,
         consumedFoods: newReading.consumedFoods,
+        foodConsumedAt: newReading.foodConsumedAt,
         description: newReading.description,
         isExercised: newReading.isExercised,
         isMedsTaken: newReading.isMedsTaken,
         userId: newReading.userId,
-        createdAt: getFormattedTimeStamp(newReading.createdAt)
-      }
-    })
-
+        createdAt: getFormattedTimeStamp(newReading.createdAt),
+      },
+    });
   } else {
-    throw new AppError('User not found', 404);
+    throw new AppError("User not found", 404);
   }
 });
 
@@ -61,7 +68,9 @@ const getAllReadings = asyncHandler(async (req, res) => {
     //Get the readings of all the foodIds passed in the query param
     const foodIds = req.query?.foodIds?.split(",");
     if (!isArrayEmpty(foodIds)) {
-      const readingsForSelectedFood = await Food.find({ "_id": { $in: foodIds } }).populate('readings');
+      const readingsForSelectedFood = await Food.find({
+        _id: { $in: foodIds },
+      }).populate("readings");
       return res.status(200).json(readingsForSelectedFood);
     }
 
@@ -69,39 +78,38 @@ const getAllReadings = asyncHandler(async (req, res) => {
     const readings = await GlucoReading.aggregate([
       {
         $match: {
-          'userId': user._id
-        }
+          userId: user._id,
+        },
       },
       {
-        $lookup:
-        {
+        $lookup: {
           from: "foods",
           localField: "consumedFoods",
           foreignField: "_id",
-          as: "consumedFoods"
-        }
+          as: "consumedFoods",
+        },
       },
       {
         $group: {
           _id: {
             $dateToString: {
               format: "%d-%m-%Y",
-              date: "$createdAt"
-            }
+              date: "$createdAt",
+            },
           },
           results: {
-            $push: "$$ROOT"
-          }
-        }
+            $push: "$$ROOT",
+          },
+        },
       },
       {
-        $sort: { '_id': -1 }
-      }
+        $sort: { _id: -1 },
+      },
     ]);
 
     res.status(200).json(readings);
   } else {
-    throw new AppError('User not found', 404);
+    throw new AppError("User not found", 404);
   }
 });
 
@@ -112,26 +120,22 @@ const getReadingsForDate = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
   const { date } = req.params;
 
-  let startDate = moment(date, "DD-MM-YYYY").startOf('day').toDate();
-  const endDate = moment(date, "DD-MM-YYYY").endOf('day').toDate();
+  let startDate = moment(date, "DD-MM-YYYY").startOf("day").toDate();
+  const endDate = moment(date, "DD-MM-YYYY").endOf("day").toDate();
 
   if (user) {
     const readings = await GlucoReading.find({
       userId: user._id,
       createdAt: {
         $gte: startDate,
-        $lt: endDate
-      }
-    }).populate('consumedFoods');
+        $lt: endDate,
+      },
+    }).populate("consumedFoods");
 
     res.status(200).json(readings);
   } else {
-    throw new AppError('User not found', 404);
+    throw new AppError("User not found", 404);
   }
 });
 
-export {
-  createReading,
-  getAllReadings,
-  getReadingsForDate
-}
+export { createReading, getAllReadings, getReadingsForDate };
